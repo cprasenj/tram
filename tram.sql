3cee2c3457208395b4a24c8f85e17753584bcb89
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2015-03-20 13:35:49 +0530

    tabils are created with basic attributes

--- tram.sql
+++ tram.sql
@@ -19,6 +19,16 @@ PROMPT '.... User created with required privileges'
 PROMPT 'Connecting as STEP user'
 conn TRAM_ADMIN/TRAM
 
+PROMPT '....monthly_holder table is in creation'
+
+CREATE TABLE monthly_holder (
+	monthly_holder_id 		NUMBER(3),
+	monthly_holder_name		VARCHAR(20),
+	CONSTRAINT monthly_holder_pk PRIMARY KEY(monthly_holder_id)
+);
+
+PROMPT '....monthly_holder table is created'
+
 PROMPT '....employee table is in creation'
 
 CREATE TABLE employee (
@@ -37,7 +47,7 @@ CREATE TABLE customer (
 	customer_name 	VARCHAR(20),
 	contact_no 		VARCHAR(15),
 	--other details also has to be captured
-	CONSTRAINT customer_pk PRIMARY KEY(employee_id)
+	CONSTRAINT customer_pk PRIMARY KEY(customer_id)
 );
 
 PROMPT '....employee table is created'
@@ -60,18 +70,7 @@ CREATE TABLE station (
 	CONSTRAINT station_pk PRIMARY KEY(station_id)
 );
 
-PROMPT '....routes table is in creation'
-
-CREATE TABLE routes (
-	route_id 			NUMBER(3),
-	start_station 		NUMBER(3),
-	end_station	        NUMBER(3),
-	CONSTRAINT route_pk PRIMARY KEY(route_id),
-	CONSTRAINT start_fk FOREIGN KEY (start_station) REFERENCES station(station_id),
-	CONSTRAINT end_fk FOREIGN KEY (end_station) REFERENCES station(station_id)  
-);
-
-PROMPT '....routes table is created'
+PROMPT '....station table is created'
 
 PROMPT '....monthly table is in creation'
 
@@ -85,18 +84,18 @@ CREATE TABLE monthly (
 
 PROMPT '....monthly table is created'
 
-PROMPT '....schedule table is in creation'
+PROMPT '....routes table is in creation'
 
-CREATE TABLE schedule (
-	schedule_id 	NUMBER(3),
-	tram_id 		NUMBER(5),
-	route_id  		Number(3),
-	CONSTRAINT schedule_pk PRIMARY KEY(schedule_id),
-	CONSTRAINT tram_fk FOREIGN KEY (tram_id) REFERENCES station(station_id),
-	CONSTRAINT route_fk FOREIGN KEY (route_id) REFERENCES station(station_id),
+CREATE TABLE routes (
+	route_id 			NUMBER(3),
+	start_station 		NUMBER(3),
+	end_station	        NUMBER(3),
+	CONSTRAINT route_pk PRIMARY KEY(route_id),
+	CONSTRAINT start_fk FOREIGN KEY (start_station) REFERENCES station(station_id),
+	CONSTRAINT end_fk FOREIGN KEY (end_station) REFERENCES station(station_id)  
 );
 
-PROMPT '....schedule table is created'
+PROMPT '....routes table is created'
 
 PROMPT '....deport table is in creation'
 
@@ -105,7 +104,7 @@ CREATE TABLE deport (
 	station_id 	NUMBER(5),
 	capacity	NUMBER(5),
 	CONSTRAINT deport_pk PRIMARY KEY(deport_id),
-	CONSTRAINT deport_id_fk FOREIGN KEY (deport_id) REFERENCES deport(deport_id)
+	CONSTRAINT deport_id_fk FOREIGN KEY (deport_id) REFERENCES station(station_id)
 );
 
 PROMPT '....deport table is created'
@@ -113,10 +112,10 @@ PROMPT '....deport table is created'
 PROMPT '....tram_type table is in creation'
 
 CREATE TABLE tram_type (
-	tram_type_id 		NUMBER(3),
+	tram_type_id 		NUMBER(5),
 	tram_type_name		VARCHAR(20),
 	fare_chart_id		NUMBER(5),
-	CONSTRAINT tram_pk PRIMARY KEY(tram_type_id)
+	CONSTRAINT tram_type_pk PRIMARY KEY(tram_type_id)
 );
 
 PROMPT '....tram_type table is created'
@@ -124,27 +123,17 @@ PROMPT '....tram_type table is created'
 PROMPT '....tram table is in creation'
 
 CREATE TABLE tram (
-	tram_id 		NUMBER(3),
+	tram_id 		NUMBER(5),
 	tram_name		VARCHAR(20),
-	tram_type_id 	VARCHAR(5),
+	type_id 		NUMBER(5),
 	fare_chart_id 	VARCHAR(5),
 	on_road_date 	DATE,
 	CONSTRAINT tram_pk PRIMARY KEY(tram_id),
 	-- CONSTRAINT fare_chart_fk FOREIGN KEY (end_station) REFERENCES station(station_id),
-	CONSTRAINT tram_type_fk FOREIGN KEY (tram_type_id) REFERENCES tram_type(tram_type_id)    
+	CONSTRAINT tram_type_fk FOREIGN KEY (type_id) REFERENCES tram_type(tram_type_id)    
 );
 
 PROMPT '....tram table is created'
-
-PROMPT '....monthly_holder table is in creation'
-
-CREATE TABLE monthly_holder (
-	monthly_holder_id 		NUMBER(3),
-	monthly_holder_name		VARCHAR(20),
-	CONSTRAINT monthly_holder_pk PRIMARY KEY(monthly_holder_id)
-);
-
-PROMPT '....monthly_holder table is created'
 
 PROMPT '....ticket table is in creation'
 
@@ -159,3 +148,16 @@ CREATE TABLE ticket (
 );
 
 PROMPT '....ticket table is created'
+
+PROMPT '....schedule table is in creation'
+
+CREATE TABLE schedule (
+	schedule_id 	NUMBER(3),
+	tram_id 		NUMBER(5),
+	route_id  		Number(3),
+	CONSTRAINT schedule_pk PRIMARY KEY(schedule_id),
+	CONSTRAINT tram_fk FOREIGN KEY (tram_id) REFERENCES station(station_id),
+	CONSTRAINT route_fk FOREIGN KEY (route_id) REFERENCES station(station_id)
+);
+
+PROMPT '....schedule table is created'
